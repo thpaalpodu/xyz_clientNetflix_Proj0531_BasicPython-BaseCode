80c243d1c6f028811717a0da9ea2d02f9bb40ae0
thpaalpodu <thpaalpodu@gmail.com>
2026-05-31 12:54:41 +0530

    Adding the local python file into the main branch
    not for now

---
+++ MyFirstProgram_24Dec25.py
@@ -0,0 +1,70 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+help
+Type help() for interactive help, or help(object) for help about object.
+3+5
+8
+
+==================================================================================== RESTART: Shell ====================================================================================
+
+==================================================================================== RESTART: Shell ====================================================================================
+y="Sathiyam"
+y
+'Sathiyam'
+clear
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+help
+Type help() for interactive help, or help(object) for help about object.
+help()
+Welcome to Python 3.14's help utility! If this is your first time using
+Python, you should definitely check out the tutorial at
+https://docs.python.org/3.14/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To get a list of available
+modules, keywords, symbols, or topics, enter "modules", "keywords",
+"symbols", or "topics".
+
+You can use the following keyboard shortcuts at the main interpreter prompt.
+F1: enter interactive help, F2: enter history browsing mode, F3: enter paste
+mode (press again to exit).
+
+Each module also comes with a one-line summary of what it does; to list
+the modules whose name or summary contain a given string such as "spam",
+enter "modules spam".
+
+To quit this help utility and return to the interpreter,
+enter "q", "quit" or "exit".
+
+help> 5**3
+No Python documentation found for '5**3'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+
+help> exit
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+>>> 5**3
+125
+>>> 8+2*10
+28
+>>> (8+2)10
+SyntaxError: invalid syntax
+>>> 18/3
+6.0
+>>> 18/4
+4.5
+>>> 18.0/4
+4.5
+>>> Saty = 'SuperMan'
+>>> print(Saty)
+SuperMan
+>>> Saty ="i am eager to learn more"
+>>> print(Saty)
+i am eager to learn more
